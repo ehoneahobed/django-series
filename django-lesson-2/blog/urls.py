b02913e58de04af9e9868f88e8db214831aa747f
ehoneahobed <ehoneahobed@hotmail.com>
2024-02-08 13:28:14 -0500

    Remove all tracked files

--- django-lesson-2/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.blog_archive, name='blog_archive'),
-    path('<int:article_id>/', views.blog_post, name='blog_post'),
-]
