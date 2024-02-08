c496a4f447df797b9d237c5b641bf23bffe3d9c3
ehoneahobed <ehoneahobed@hotmail.com>
2024-02-08 13:37:51 -0500

    Remove all tracked files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Django Series - Learn With Ehoneah
-Find all the code written during the live sessions for the django series here.
--- django-lesson-2/blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:article_id>/', views.blog, name='blog'),
-]
